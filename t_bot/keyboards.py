3f9db096663684d7c7d1dcfd55d29035f4496438
bunyod <naimov.bunyod2021@gmail.com>
2024-05-02 16:36:36 +0500

    update project

--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-web_app = WebAppInfo(url='https://www.google.com')
+web_app = WebAppInfo(url='https://bunyodnaimov.github.io/modul_11.github.io/')
 
 app_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Mini App', web_app=web_app)]
