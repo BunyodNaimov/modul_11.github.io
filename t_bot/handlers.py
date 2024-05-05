2769bef83a39be57a0c655bbc402407d63978fea
bunyod <naimov.bunyod2021@gmail.com>
2024-05-05 21:51:54 +0500

    update project

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -1,4 +1,5 @@
-from aiogram import Dispatcher, types
+from aiogram import Dispatcher, types, F
+from aiogram.enums import ContentType
 from aiogram.filters import CommandStart
 
 from keyboards import app_kb
@@ -9,3 +10,8 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(msg: types.Message):
     await msg.answer("Salom", reply_markup=app_kb)
+
+
+@dp.message(F.content_type.in_(ContentType.WEB_APP_DATA, ))
+async def say(msg: types.Message):
+    await msg.answer(msg.web_app_data.data)
