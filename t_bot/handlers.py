c3a1da2fbd93ddd86c9b8a99676ff4631095b823
bunyod <naimov.bunyod2021@gmail.com>
2024-05-05 23:26:01 +0500

    update

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher, types, F
 from aiogram.enums import ContentType
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, MagicData, callback_data
+from aiogram.utils.magic_filter import MagicFilter
 
 from keyboards import app_kb
 
@@ -12,8 +13,7 @@ async def start(msg: types.Message):
     await msg.answer("Salom", reply_markup=app_kb)
 
 
-@dp.message(F)
+@dp.message(F.func(lambda msg: msg.web_app_data.data == "TestMessage"))
 async def say(msg: types.Message):
-    print(msg)
-    print('hello')
+
     await msg.answer(msg.web_app_data.data)
