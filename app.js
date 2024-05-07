788a85f0b8c3f59e448a613dcf5ee779e8e93d01
bunyod <naimov.bunyod2021@gmail.com>
2024-05-07 20:35:21 +0500

    update now

--- app.js
+++ app.js
@@ -11,20 +11,40 @@ let btn4 = document.getElementById("btn4")
 let btn5 = document.getElementById("btn5")
 let btn6 = document.getElementById("btn6")
 
+
 btn1.addEventListener("click", function () {
     tg.MainButton.setText("btn1 bosildi");
     tg.MainButton.show();
-}
-)
+})
+
+
+btn1.addEventListener("click", function () {
+    tg.MainButton.setText("btn1 bosildi");
+    tg.MainButton.show();
+})
+btn1.addEventListener("click", function () {
+    tg.MainButton.setText("btn1 bosildi");
+    tg.MainButton.show();
+})
 btn2.addEventListener("click", function () {
     tg.MainButton.setText("btn2 bosildi");
     tg.MainButton.show();
-}
-)
+})
 btn3.addEventListener("click", function () {
     tg.MainButton.setText("btn3 bosildi");
     tg.MainButton.show();
-}
-)
+})
+btn4.addEventListener("click", function () {
+    tg.MainButton.setText("btn4 bosildi");
+    tg.MainButton.show();
+})
+btn5.addEventListener("click", function () {
+    tg.MainButton.setText("btn5 bosildi");
+    tg.MainButton.show();
+})
+btn6.addEventListener("click", function () {
+    tg.MainButton.setText("btn6 bosildi");
+    tg.MainButton.show();
+})
 
 
