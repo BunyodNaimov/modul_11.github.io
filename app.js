5e87919245b95444be320e3f7be7f174d58aab7b
bunyod <naimov.bunyod2021@gmail.com>
2024-05-09 16:14:38 +0500

    update now

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
 
 let item = " "
 
@@ -34,18 +32,6 @@ btn3.addEventListener("click", function () {
 btn4.addEventListener("click", function () {
     tg.MainButton.setText("btn4 bosildi");
     item = "button4 bosildi";
-
-    tg.MainButton.show();
-});
-btn5.addEventListener("click", function () {
-    tg.MainButton.setText("btn5 bosildi");
-    item = "button5 bosildi";
-
-    tg.MainButton.show();
-});
-btn6.addEventListener("click", function () {
-    tg.MainButton.setText("btn6 bosildi");
-    item = "button6 bosildi";
 
     tg.MainButton.show();
 });
