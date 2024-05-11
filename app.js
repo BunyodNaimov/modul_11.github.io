f7f722f50f7ebd8ed3a1c4846c2ba4b2237b28f8
bunyod <naimov.bunyod2021@gmail.com>
2024-05-11 11:59:19 +0500

    update now

--- app.js
+++ app.js
@@ -7,48 +7,28 @@ tg.MainButton.color = "#FC3005"
 let rbtn1 = document.getElementById("rbtn1");
 let abtn1 = document.getElementById("abtn1");
 
-let rbtn2 = document.getElementById("rbtn2");
-let abtn2 = document.getElementById("abtn2");
-
-let rbtn3 = document.getElementById("rbtn3");
-let abtn3 = document.getElementById("abtn3");
-
-let rbtn4 = document.getElementById("rbtn4");
-let abtn4 = document.getElementById("abtn4");
 
 let item = " "
-let num_count = 0
+let n_count1 = 0
 
-number = document.getElementById("count");
+
+count1 = document.getElementById("count1");
+count2 = document.getElementById("count2");
+count3 = document.getElementById("count3");
+count4 = document.getElementById("count4");
 
 abtn1.addEventListener("click", function () {
-
-    number.innerText = num_count += 1;
-    number.style.display = "block";
+    count1.innerText = n_count1 += 1;
+    count1.style.display = "inline-block";
+    tg.MainButton.setText("Burger")
+    item = "Burger/"+count1
+})
+rbtn1.addEventListener("click", function () {
+    count1.innerText = n_count1 -= 1;
+    count1.style.display = "inline-block";
+    item = "Burger/"+count1
 })
 
-
-btn1.addEventListener("click", function () {
-    tg.MainButton.setText("Burger " + num);
-    item = "Burger/3$";
-    tg.MainButton.show();
-});
-btn2.addEventListener("click", function () {
-    tg.MainButton.setText("KFC");
-    item = "KFC/10$";
-    tg.MainButton.show();
-});
-btn3.addEventListener("click", function () {
-    tg.MainButton.setText("LAVASH");
-    item = "LAVASH/4$";
-    tg.MainButton.show();
-});
-btn4.addEventListener("click", function () {
-    tg.MainButton.setText("Pizza");
-    item = "Pizza/5$";
-
-    tg.MainButton.show();
-});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     tg.sendData(item);
