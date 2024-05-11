0bb9ea66e84a0eae4816e1630e27cbdfeb8d9ad5
bunyod <naimov.bunyod2021@gmail.com>
2024-05-11 10:56:10 +0500

    update now

--- app.js
+++ app.js
@@ -4,17 +4,32 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF"
 tg.MainButton.color = "#FC3005"
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
+let rbtn1 = document.getElementById("rbtn1");
+let abtn1 = document.getElementById("abtn1");
+
+let rbtn2 = document.getElementById("rbtn2");
+let abtn2 = document.getElementById("abtn2");
+
+let rbtn3 = document.getElementById("rbtn3");
+let abtn3 = document.getElementById("abtn3");
+
+let rbtn4 = document.getElementById("rbtn4");
+let abtn4 = document.getElementById("abtn4");
 
 let item = " "
-let num = 1
+let num_count = 0
+
+number = document.getElementById("count");
+
+abtn1.addEventListener("click", function () {
+
+    number.innerText = num_count += 1;
+    number.style.display = "block";
+})
 
 
 btn1.addEventListener("click", function () {
-    tg.MainButton.setText("Burger "+ num);
+    tg.MainButton.setText("Burger " + num);
     item = "Burger/3$";
     tg.MainButton.show();
 });
