da57f3a28bd522fb5dbb8f9367bdc0a8709fc8cc
bunyod <naimov.bunyod2021@gmail.com>
2024-05-25 16:47:22 +0500

    the end

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -49,8 +49,8 @@ async def get_btn(msg: Message):
         prices=[LabeledPrice(label=f"{product["title"]}({product["quantity"]})",
                              amount=(product["price"] * product["quantity"]) * 100)
                 for product in products.values()],
-        max_tip_amount=500000,
-        suggested_tip_amounts=[10000, 30000, 50000, 60000]  # Chayeviy
+        max_tip_amount=50000000,
+        suggested_tip_amounts=[100000, 300000, 500000, 600000]  # Chayeviy
 
     )
 
